1f002baf518276e2d3fd23e13325b51062200fa0
dannysepler <dsepler@ufl.edu>
2014-10-20 00:26:13 -0400

    made changes to syntax to make UI better

--- emails.py
+++ emails.py
@@ -2,21 +2,31 @@ import sys
 import csv
 import tkFileDialog
 
-# ans = input("drag and drop the csv file into the terminal. press enter to continue")
+print '\n'
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+print 'welcome to the email grabber!'
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+print ''
+print 'i\'m going to ask you for the csv file to enter. got it?'
+try:
+    raw_input('(press enter to continue)')
+except SyntaxError:
+    pass
+
 f = tkFileDialog.askopenfilename()
 
-print "looks like you chose this file: " + f
+print 'looks like you chose this file: ' + f + '\n'
 file_input = open(f, "r")
 
 
-print "i'll be printing to \"output.txt\""
-file_output = open("output.txt", "w")
+print 'i\'ll be printing to \"output.txt\"\n'
+file_output = open('output.txt', 'w')
 
 counter = 0
 
 for line in file_input:
 	if counter > 2:
-		currentline = line.split(",")
+		currentline = line.split(',')
 		file_output.write( currentline[1] + '@ufl.edu, ')
 	counter += + 1
 
