4f9c646216c59d09b7da0163cc60491d50cfbdbd
dannysepler <dsepler@ufl.edu>
2014-10-20 00:46:44 -0400

    changed print statements

--- emails.py
+++ emails.py
@@ -19,16 +19,20 @@ print 'looks like you chose this file: ' + f + '\n'
 file_input = open(f, "r")
 
 
-print 'i\'ll be printing to \"output.txt\"\n'
+print 'i\'ll be printing to \"output.txt\"'
 file_output = open('output.txt', 'w')
 
 counter = 0
+
+sys.stdout.write('loading')
 
 for line in file_input:
 	if counter > 2:
 		currentline = line.split(',')
 		file_output.write( currentline[1] + '@ufl.edu, ')
+		sys.stdout.write('.')
 	counter += 1
 
+print ' done!\n'
 file_input.close()
 file_output.close()
